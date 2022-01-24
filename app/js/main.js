9a412b80cfc5092817503a72c90ec12e56b8a3b8
zur1k <zazamuradov@gmail.com>
2022-01-25 00:14:48 +0200

    addaptive for mobile 3 block

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
+// ------------------------ MONILE NAV -------------------------
+const mobileNavButton = document.querySelector('.mobile-nav-button')
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon')
+const mobileNav = document.querySelector('.mobile-nav')
+
+mobileNavButton.addEventListener('click', function () {
+	mobileNavIcon.classList.toggle('active');
+	mobileNav.classList.toggle('active')
+	document.body.classList.toggle('no-scroll')
+
+})
+
+
+$(".mobile-nav__link").on("click", function (event) {
+	//
+
+	$(mobileNavIcon).toggleClass("active");
+	$(mobileNav).toggleClass("active");
+	document.body.classList.remove('no-scroll')
+});
+
+// slider !!!!
 $(document).ready(function () {
 	$('.testimoni__slider').slick({
 		arrows: false,
